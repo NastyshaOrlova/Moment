38f792db9a5737fe811f491c1bf028a8c64589d4
Nastya Orlova <web.orlova.dev@gmail.com>
2025-08-15 11:06:41 +0300

    feat: Подключение redux-persist для сохранения store в AsyncStorage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,19 @@
+import { Loading } from "@/components/widgets";
 import { Stack } from "expo-router";
 import { Provider } from "react-redux";
-import { store } from "../store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "../store";
 
 export default function RootLayout() {
   return (
     <Provider store={store}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="day/[dayName]" options={{ headerShown: false }} />
-        <Stack.Screen name="all-days" options={{ headerShown: false }} />
-      </Stack>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="day/[dayName]" options={{ headerShown: false }} />
+          <Stack.Screen name="all-days" options={{ headerShown: false }} />
+        </Stack>
+      </PersistGate>
     </Provider>
   );
 }
--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,28 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
 import diaryReducer from "./slices/diarySlice";
+
+const persistConfig = {
+  key: "diary",
+  storage: AsyncStorage,
+};
+
+const persistedDiaryReducer = persistReducer(persistConfig, diaryReducer);
 
 export const store = configureStore({
   reducer: {
-    diary: diaryReducer,
+    diary: persistedDiaryReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
